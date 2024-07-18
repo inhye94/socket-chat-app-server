583470590d96db7a542fac2241d4c044660bf274
DuetoPark <inhye.park.dev@gmail.com>
2024-07-18 23:49:16 +0900

    ✨ temp plus/minus event 구현

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import { addUser, removeUser, getUser, getUsersInRoom } from "./users.js";
 import router from "./router.js";
 
 const PORT = process.env.PORT || 8080;
+let temp = 10;
 
 const app = express();
 app.use(cors());
@@ -62,6 +63,72 @@ io.on("connection", (socket) => {
     });
 
     callback();
+  });
+
+  // NOTE: air 방에 진입
+  socket.on("joinAir", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    // 룸에 들어가기
+    socket.join(user.room);
+
+    console.log(`${user.name} has joined! (room: ${user.room})`);
+
+    // initTemp 알려주기
+    io.to(user.room).emit("initTemp", temp);
+
+    callback();
+  });
+
+  // NOTE: plus temp
+  socket.on("plusTemp", ({ name, room }) => {
+    let user = getUser(socket.id);
+
+    if (!user) {
+      const { user: userData } = addUser({ id: socket.id, name, room });
+
+      user = { ...userData };
+
+      // 룸에 들어가기
+      socket.join(user.room);
+
+      console.log(`${user.name} has joined! (room: ${user.room})`);
+    }
+
+    if (temp < 30) {
+      temp += 1;
+      console.log(`${user.name} change temp. (temp: ${temp})`);
+    }
+
+    io.to(user.room).emit("tempChange", { username: user.name, temp });
+  });
+
+  // NOTE: minus temp
+  socket.on("minusTemp", ({ name, room }) => {
+    let user = getUser(socket.id);
+
+    if (!user) {
+      const { user: userData } = addUser({ id: socket.id, name, room });
+
+      user = { ...userData };
+
+      // 룸에 들어가기
+      socket.join(user.room);
+
+      console.log(`${user.name} has joined! (room: ${user.room})`);
+    }
+
+    if (temp > 0) {
+      temp -= 1;
+      console.log(`${user.name} change temp. (temp: ${temp})`);
+    }
+
+    io.to(user.room).emit("tempChange", { username: user.name, temp });
   });
 
   socket.on("disconnect", () => {
